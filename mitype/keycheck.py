661ec4c4f54d7483a24ccc9bead7f54a602fdd16
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2021-01-14 17:53:50 +0530

    Add feature to share result on Twitter (#74)
    Detect Ctrl+t keypress and share test result
    on twitter.

--- mitype/keycheck.py
+++ mitype/keycheck.py
@@ -39,6 +39,18 @@ def is_ctrl_c(key):
     return key == "\x03"
 
 
+def is_ctrl_t(key):
+    """Detects Ctrl+t key combination.
+    This is used to share results on twitter
+    Args:
+        key (string):
+    Returns:
+        bool: Return true if Ctrl+t is pressed.
+            Return false otherwise
+    """
+    return key == "\x14"
+
+
 def is_backspace(key):
     """Detect BACKSPACE key.
 
