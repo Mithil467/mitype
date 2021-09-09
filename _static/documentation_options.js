fcdbf59b6ec2146b60cd889ac0cdf2a5ed02d7d9
Mithil Poojary <mithil467@gmail.com>
2021-09-09 10:38:17 +0000

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g42747b8',
+    VERSION: '0+untagged.1.gd03b5d3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
