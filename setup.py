0caa62b8af91e65b3dec2b361db5f61ad6c4fa10
Dark-Knight11 <vedantbhamare11@gmail.com>
2021-10-09 21:18:44 +0530

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup
 
 import versioneer
 
-setup(
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-)
+if __name__ == "__main__":
+    setup(
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        download_url="https://github.com/mithil467/mitype/archive/v%s.tar.gz"
+        % versioneer.get_version(),
+    )
